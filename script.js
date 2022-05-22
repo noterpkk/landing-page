22cdfd90f816d14161c61dff129907a3ac6e16ab
noterpkk <106000830+noterpkk@users.noreply.github.com>
2022-05-22 18:52:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 $("img").attr("draggable","false")
-$(".quadrado").css("transform","translate(0px) scale(0.8)").css("opacity","0.8")
+$(".bg1-main").css("transform","translate(0px) scale(0.8)").css("opacity","0.8")
         setTimeout(() => {
-            $(".quadrado").css("transform","translate(0px) scale(1)").css("opacity","1")
+            $(".bg1-main").css("transform","translate(0px) scale(1)").css("opacity","1")
         }, 120);
+        $(".bg2-main").css("transform","translate(0px) scale(0.8)").css("opacity","0.8")
+        setTimeout(() => {
+            $(".bg2-main").css("transform","translate(0px) scale(1)").css("opacity","1")
+        }, 120);
+        $(".bg3-main").css("transform","translate(0px) scale(0.8)").css("opacity","0.8")
+        setTimeout(() => {
+            $(".bg3-main").css("transform","translate(0px) scale(1)").css("opacity","1")
+        }, 120);
+        $(".bg4-main").css("transform","translate(0px) scale(0.8)").css("opacity","0.8")
+        setTimeout(() => {
+            $(".bg4-main").css("transform","translate(0px) scale(1)").css("opacity","1")
+        }, 120);
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    return false;
+    }
+        }
