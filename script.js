7fd5e2bfbaf24500f227e2c1418535ff092bac0f
noterpkk <106000830+noterpkk@users.noreply.github.com>
2022-05-27 22:57:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,20 +15,33 @@ $(".bg1-main").css("transform","translate(0px) scale(0.8)").css("opacity","0.8")
         setTimeout(() => {
             $(".bg4-main").css("transform","translate(0px) scale(1)").css("opacity","1")
         }, 120);
-document.onkeydown = function(e) {
-if(event.keyCode == 123) {
-return false;
-}
-if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-return false;
-}
-if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-return false;
-}
-if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-return false;
-}
-if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-    return false;
-    }
-        }
+        $(".bg5-main").css("transform","translate(0px) scale(0.8)").css("opacity","0.8")
+        setTimeout(() => {
+            $(".bg5-main").css("transform","translate(0px) scale(1)").css("opacity","1")
+        }, 120);
+        $(".bg6-main").css("transform","translate(0px) scale(0.8)").css("opacity","0.8")
+        setTimeout(() => {
+            $(".bg6-main").css("transform","translate(0px) scale(1)").css("opacity","1")
+        }, 120);
+        $(".bg7-main").css("transform","translate(0px) scale(0.8)").css("opacity","0.8")
+        setTimeout(() => {
+            $(".bg7-main").css("transform","translate(0px) scale(1)").css("opacity","1")
+        }, 120);
+
+        document.onkeydown = function(e) {
+            if(event.keyCode == 123) {
+               return false;
+            }
+            if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+               return false;
+            }
+            if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+               return false;
+            }
+            if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+               return false;
+            }
+            if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+               return false;
+            }
+          }
